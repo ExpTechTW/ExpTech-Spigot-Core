4a93df67e76d05692c1a71618229665c4cdf9e03
whes1015 <whes1015@gmail.com>
2022-07-22 16:42:40 +0800

    feat: init

---
+++ src/main/java/main.java
@@ -0,0 +1,10 @@
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
